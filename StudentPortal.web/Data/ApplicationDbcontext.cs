821f650f8b748ee60f696fa53aca433fcff9f11a
wajhi <wajhi.bzns2024@gmail.com>
2026-06-03 14:46:26 +0500

    Add project files.

---
+++ StudentPortal.web/Data/ApplicationDbcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortal.web.Models.Entities;
+
+namespace StudentPortal.web.Data
+{
+    public class ApplicationDbcontext: DbContext
+    {
+        public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext>options):base(options)   //constructor
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentPortal.web/Models/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentPortal.web.Models.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
